3be1d0fe4afcdd88805c939c6195eee795dba45e
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-19 16:47:41 +0100

    car kotlin class

---
+++ app/src/main/java/pl/apka/javafaithful/Car.kt
@@ -0,0 +1,3 @@
+package pl.apka.javafaithful
+
+class Car(val name: String, val year: Int)
--- app/src/main/java/pl/apka/javafaithful/MainActivity.java
+++ app/src/main/java/pl/apka/javafaithful/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Car myCar = new Car("Fiat",1978);
+        
     }
 }
